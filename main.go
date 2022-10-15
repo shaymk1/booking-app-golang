7809a5f6233f775b850c330ddcf8195a03675828
unknown <mkekae@gmail.com>
2022-10-15 07:56:07 +0200

    initiated go.mod and adding variables to main function, impoted fmt

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main (){
+
+var conferenceName = "Go Conference"
+const conferenceTickets = 50
+var remainingTickets = 50
+fmt.Println("welcome to our", conferenceName, "booking app")
+fmt.Println("we have", conferenceTickets, "tickets in total and we now have", remainingTickets, "tickets left")
+fmt.Println("Get your tickets here to now!")
+
+// ticket-booking
+
+
+
+}
